18235ff272faf3dc505779419e4dbf93e09cd204
OdehYazan <eng.yazanodeh@gmail.com>
2021-07-04 18:10:23 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React from 'react'
-import {form,button} from 'react-bootstrap';
-import axios from 'axios'
-import { render } from '@testing-library/react';
+import { Form, Button } from 'react-bootstrap'
+import Axios from 'axios'
 
-class App extends  React.Component{
-  constructor(props){
+
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state ={
-      cityInfo :{}
-
+    this.state = {
+      cityInfo: {},
+      userInputForCityName: '',
+      showMap: false
     }
-    
-  }
-getlocation = async () =>{
-  let url='https://us1.locationiq.com/v1/search.php?key=pk.891ff00d5f4e98e578a4be3cb6478d75&q=Amman&format=json'
-}
-  
 
-render(){
-  return(
-    <div>
-      <h1>City Explorer</h1>
-    </div>
-  )
-}
+  }
+
+
+  getLocation = async (event) => {
+
+    event.preventDefault();
+
+    await this.setState({
+
+      userInputForCityName: event.target.City.value
+
+    })
+
+    console.log(this.state.userInputForCityName);
+
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_LOCATION_KEY}&q=${this.state.userInputForCityName}&format=json`
+
+    let dataArray = await Axios.get(url);
+
+    console.log(dataArray)
+    console.log(dataArray.data[0])
+
+    this.setState({
+
+
+      cityInfo: dataArray.data[0],
+      showMap: true
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <Form onSubmit={this.getLocation}>
+          <label > City Name :  </label>
+          <Form.Control size="lg" type="text" placeholder="Enter City name" name='City' />
+          <Button variant="primary" type="submit">Explore!</Button>
+
+        </Form>
+
+        <h3>City Information</h3>
+        <h4>City Name :{this.state.cityInfo.display_name}</h4>
+        <h5> latitude :{this.state.cityInfo.lat} /longitude :{this.state.cityInfo.lon}</h5>
+        {this.state.showMap &&
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_LOCATION_KEY}&center=${this.state.cityInfo.lat},${this.state.cityInfo.lon}&zoom=20&size=400x400&format=jpg&maptype=roadmap`} alt='map' />}
+      </div>
+    )
+  }
 }
 export default App;
