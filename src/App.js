280c6af584ce24232fea12a8bf5363788e389258
OdehYazan <eng.yazanodeh@gmail.com>
2021-07-04 16:19:51 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {form,button} from 'react-bootstrap';
+import axios from 'axios'
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      cityInfo :{}
+
+    }
+    
+  }
+getlocation = async () =>{
+  let url='https://us1.locationiq.com/v1/search.php?key=pk.891ff00d5f4e98e578a4be3cb6478d75&q=Amman&format=json'
 }
+  
 
+render(){
+  return(
+    <div>
+      <h1>City Explorer</h1>
+    </div>
+  )
+}
+}
 export default App;
